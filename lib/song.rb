8182d92b72a621ec84b0a9532a7bb08cc6f9dca7
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-11 09:27:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
     @@genres.each do |i|
       if(!unique.include?(i))
         unique << i
-        hash[i] = @@genre_count.count(i)
+        hash[i] = @@genre.count(i)
         binding.pry
       end
     end
